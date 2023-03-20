5cb89f1bfd88854a0bb6fd44bfcd23340318b6e4
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-03-20 12:16:38 +0100

    Fixed scss clashing errors

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -14,7 +14,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="card-list">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted" type="button" onClick={addToCartHandler}>
         Add To Cart
