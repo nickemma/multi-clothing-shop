92b03e6eedcc9f2e9cf050a4913ec0d0f7c6c93d
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-03-22 19:48:58 +0100

    Refactored the cart context to utilize redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './user/userReducer';
 import { categoriesReducer } from './categories/categoriesReducer';
+import { cartReducer } from './cart/cartReducer';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   categories: categoriesReducer,
+  cart: cartReducer,
 });
