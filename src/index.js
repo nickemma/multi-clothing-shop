9ce72aeb954c02f2442b3e699d837bbe38247d72
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-03-27 14:49:06 +0100

    Added the stripe backend for serveless

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import { store, persistor } from './redux/configureStore';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.config';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +15,9 @@ root.render(
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
