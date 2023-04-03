d2439ed3e479cb976adaaf6e945625acc69b1e0c
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-03 11:19:47 +0100

    Added code splitting and lazy loading

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,20 +1,24 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navigation from '../components/navigation/Navigation';
-import Authentication from '../pages/auth/Authentication';
-import Checkout from '../pages/checkout/Checkout';
-import Home from '../pages/home/Home';
-import Shop from '../pages/shop/Shop';
+import Spinner from '../components/spinner/Spinner';
+const Navigation = lazy(() => import('../components/navigation/Navigation'));
+const Authentication = lazy(() => import('../pages/auth/Authentication'));
+const Checkout = lazy(() => import('../pages/checkout/Checkout'));
+const Home = lazy(() => import('../pages/home/Home'));
+const Shop = lazy(() => import('../pages/shop/Shop'));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
